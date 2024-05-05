6185aa53740b04e431b82434874906c7f7dd6a6b
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-05-05 16:48:37 -0300

    initial commit

---
+++ src/main/java/com/example/salesIntel/SalesIntelApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesIntel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesIntelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesIntelApplication.class, args);
+	}
+
+}
