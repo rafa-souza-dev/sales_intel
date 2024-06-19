ceb2b2f162aa03ae8e5c723a63e50a1548818173
amadeusantos <amadeusantos31@gmail.com>
2024-06-19 16:27:51 -0300

    [fix] remove salesProducts and simplification of the sales and user relationship

--- src/main/java/com/example/salesIntel/repository/SalesProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.salesIntel.repository;
-
-import com.example.salesIntel.model.SalesProducts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesProductsRepository extends JpaRepository<SalesProducts, Long> {
-}
